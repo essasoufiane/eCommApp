21b82e6c6fe939bb16304ce118248b4e9b05fc2b
Soufiane Essaouari <soufiane.pro@yahoo.com>
2023-02-15 22:16:02 +0100

    add Choix Address dans tunnel de vente

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\OrderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,7 +12,16 @@ class OrderController extends AbstractController
     #[Route('/commande', name: 'app_order')]
     public function index(): Response
     {
+        if (!$this->getUser()->getAddresses()->getValues()) {
+            return $this->redirectToRoute('app_address_new');
+        }
+
+        $form = $this->createForm(OrderType::class, null, [
+            'user' =>$this->getUser(),//Pour récuperer uniquement les adresses du User connecté.
+        ]);
+
         return $this->render('order/index.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
